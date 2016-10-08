c6cc8b2408cc8713143f372d5720bef2cc6a7143
Porrith Suong <psuong95@gmail.com>
2016-10-08 18:15:45 -0400

    Adds write to file.

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+from twilio import twiml
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return
 
+@app.route("/receive", methods=['POST'])
+def receive_sms():
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    print "From: " + number + " | Message: " + message_body
+    write_to_file(number, message_body)
+
+def write_to_file(number, message):
+    with open("queue.json", "w") as outfile:
+        data = json.dump(
+            {
+                str(number) : str(message)
+            },
+            outfile
+        )
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
