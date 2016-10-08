6f529716229b17c43d8bbfffd58dae0de6933db6
Porrith Suong <psuong95@gmail.com>
2016-10-08 17:10:20 -0400

    Adds flask.

---
+++ Assets/Scripts/PlayingField.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayingField: MonoBehaviour
+{
+	public int xDimension = 10;
+	public int yDimension = 5;
+
+	private void Start ()
+	{
+		
+	}
+}
