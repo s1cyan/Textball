51a6bc5d60db8b9caf69a67ab8e46292ddaa22af
Porrith Suong <psuong95@gmail.com>
2016-10-08 21:11:56 -0400

    Adds logic for queuing and updating.

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return
+
 
 @app.route("/receive", methods=['POST'])
 def receive_sms():
@@ -14,16 +16,26 @@ def receive_sms():
     message_body = request.form['Body']
 
     print "From: " + number + " | Message: " + message_body
-    write_to_file(number, message_body)
+    queue_or_update(number, message_body)
 
-def write_to_file(number, message):
-    with open("queue.json", "w") as outfile:
-        data = json.dump(
-            {
-                str(number) : str(message)
-            },
-            outfile
-        )
-    
+
+def queue_or_update(number, message):
+    # with open("queue.json", "r+") as json_file:
+    #     data = json.load(json_file)
+    #     data['players'].update({ number : message })
+    #     json.dump(data, json_file)
+    json_file = open("queue.json", "r")
+    data = json.load(json_file)
+    json_file.close()
+
+    print len(data['players'])
+
+    if len(data['players']) < 8:
+        data['players'].update({number: message})
+        written_file = open("queue.json", "r+")
+        json.dump(data, written_file)
+        written_file.close()
+
+
 if __name__ == '__main__':
     app.run(port=5000)
