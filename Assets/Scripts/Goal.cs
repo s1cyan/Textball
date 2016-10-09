73fa026b9d51fc6581a607490032c1007e9ec083
Porrith Suong <psuong95@gmail.com>
2016-10-08 23:04:53 -0400

    Adds database for scoring.

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider))]
+public class Goal : MonoBehaviour
+{
+
+	public string tagToCompare = "Ball";
+	[SerializeField]
+	private ScoreDatabase teamScore;
+	private Collider collider;
+
+	private void Start ()
+	{
+		collider = GetComponent<Collider> ();
+		SetUpCollider ();
+	}
+
+	private void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag (tagToCompare)) {
+			teamScore.Earn ();
+		}
+	}
+
+	private void SetUpCollider ()
+	{
+		collider.isTrigger = true;
+	}
+}
---
+++ Assets/Scripts/ScoreDatabase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu (fileName = "Score Database", menuName = "Scriptable Objects/Score Database")]
+public class ScoreDatabase : ScriptableObject
+{
+	[SerializeField]
+	private int score = 0;
+
+	public void Earn ()
+	{
+		score += 1;
+	}
+
+	public void Lose ()
+	{
+		score -= 1;
+	}
+}
